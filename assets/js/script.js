47f30654a36e522fd7dc8715d83f37eddcde9863
Kerry Tolhurst <k_tolhurst@hotmail.co.uk>
2023-10-22 16:56:27 +0000

    Add code for the division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,10 @@ function runGame(gameType) {
 
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === 'division'){
+
+        displayDivisionQuestion((num1*num2), num2);
+
     } else {
 
         alert(`Unknown game type: ${gameType}`);
@@ -115,6 +119,10 @@ function calculateCorrectAnswer() {
 
         return [operand1 - operand2, 'subtract'];
 
+    } else if (operator === '/'){
+
+        return [operand1 / operand2, 'division'];
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplement oeprator ${operator}. Aborting!`;
@@ -165,4 +173,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    // Using ternary operator to put the largest of the two operands first to avoid negative answers
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
